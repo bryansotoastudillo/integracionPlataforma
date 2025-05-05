6cb91b0ca224048c8223bc0fb42c57de26847527
bryansotoastudillo <bryan.soto@profesor.duoc.cl>
2025-05-05 13:03:29 -0400

    Alumnos

--- src/main/java/cl/duoc/integracionPlataforma/controller/AlumnoController.java
+++ src/main/java/cl/duoc/integracionPlataforma/controller/AlumnoController.java
@@ -1,9 +1,13 @@
 package cl.duoc.integracionPlataforma.controller;
 
+import cl.duoc.integracionPlataforma.models.Alumno;
 import cl.duoc.integracionPlataforma.repository.AlumnoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/alumnos")
@@ -12,5 +16,9 @@ public class AlumnoController {
     @Autowired
     private AlumnoRepository alumnoRepository;
 
+    @GetMapping
+    public List<Alumno> getAlumnos() {
+        return alumnoRepository.findAll();
+    }
 
 }
--- src/main/java/cl/duoc/integracionPlataforma/repository/AlumnoRepository.java
+++ src/main/java/cl/duoc/integracionPlataforma/repository/AlumnoRepository.java
@@ -2,6 +2,10 @@ package cl.duoc.integracionPlataforma.repository;
 
 import cl.duoc.integracionPlataforma.models.Alumno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AlumnoRepository extends JpaRepository <Alumno,Integer> {
+
+
 }
