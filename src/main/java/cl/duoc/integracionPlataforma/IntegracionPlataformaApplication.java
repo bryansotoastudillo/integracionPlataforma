d32ce0d61965bd8d2b76a1ab193b30ba357128dd
bryansotoastudillo <bryan.soto@profesor.duoc.cl>
2025-05-05 00:35:50 -0400

    Conexión BD

---
+++ src/main/java/cl/duoc/integracionPlataforma/IntegracionPlataformaApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.integracionPlataforma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracionPlataformaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracionPlataformaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/integracionPlataforma/IntegracionPlataformaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.integracionPlataforma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracionPlataformaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
